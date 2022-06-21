86df3e56437d693e9129bcfe15482e3ed94a5058
Facundo Frias <97521604+FacundoEFrias@users.noreply.github.com>
2022-06-21 21:09:13 +0200

    Add files via upload

---
+++ PI-Food-main/api/src/routes/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const AllRecipe = require('./recipes');
+const Types = require("./types")
+const Create = require("./create")
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.use('/recipes', AllRecipe);
+router.use("/types", Types)
+router.use("/create", Create)
+
+
+module.exports = router;
