9f412042b67fd76cf97bedeed8a5667b667878d9
Facundo Frias <97521604+FacundoEFrias@users.noreply.github.com>
2022-07-08 23:00:16 +0200

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,10 @@ import {Provider} from "react-redux"
 import store from "./Redux/store"
 import axios from 'axios';
 
+const REACT_APP_API = "https://food-h.herokuapp.com"
 
-axios.defaults.baseURL = "http://localhost:3001";
+
+axios.defaults.baseURL = REACT_APP_API || "http://localhost:3001";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
